ad399bf75480565ee989b50032ac65cc10895fb1
Micki <110211211+projectsbyunique@users.noreply.github.com>
2022-07-29 10:13:12 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -3,4 +3,19 @@ tiles.setCurrentTilemap(tilemap`level`)
 Render.setAttribute(Render.attribute.wallZScale, 2)
 Render.setViewAngleInDegree(-90)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 49))
-Render.moveWithController(6, 1, 0)
+let cockpit = sprites.create(assets.image`myImage`, SpriteKind.Projectile)
+let textSprite = textsprite.create("aSasa")
+textSprite.setPosition(80, 60)
+textSprite.setFlag(SpriteFlag.RelativeToCamera, true)
+cockpit.setFlag(SpriteFlag.RelativeToCamera, true)
+Render.moveWithController(0, 0, 0)
+mySprite.setVelocity(0, -50)
+game.onUpdate(function () {
+    if (controller.left.isPressed()) {
+        mySprite.vx = -50
+    } else if (controller.right.isPressed()) {
+        mySprite.vx = 50
+    } else {
+        mySprite.vx = 0
+    }
+})
